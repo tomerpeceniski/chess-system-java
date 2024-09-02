cf3269926473867a7fa82a97ee0e42ca2359ab31
Tomer Peceniski <tomerpeceniski@gmail.com>
2024-09-02 15:05:51 +0300

    Creating methods to move pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,31 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
     public static void main(String[] args) {
-        ChessMatch chessmatch = new ChessMatch();
-        UI.printBoard(chessmatch.getPieces());
+        Scanner sc = new Scanner(System.in);
+
+        ChessMatch chessMatch = new ChessMatch();
+        int i = 0;
+        while (i < 5) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            i++;
+        }
+    sc.close();
     }
 
 }
